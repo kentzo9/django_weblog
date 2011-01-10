68721c61b41e4b27091df0acbe516a27ac0fb99f
kentzo <kentzo@gmail.com>
2011-01-10 23:46:08 +0800

    second commit,until chapter 5.4

--- Codes/django/coltrane/urls.py
+++ Codes/django/coltrane/urls.py
@@ -1,23 +1,51 @@
 from django.conf.urls.defaults import *
-from coltrane.models import Entry
+from coltrane.models import Category, Entry, Link
 
 entry_info_dict = {
    'queryset' : Entry.objects.all(),
    'date_field': 'pub_date',
-   'template_object_name' : 'entry',
 }
-
-entry_info_dict1 = {
+entry_info_dict_index = {
    'queryset' : Entry.objects.all(),
    'date_field': 'pub_date',
    'template_name' : 'coltrane/entry_index.html', #this will replace the default template, which is entry_archive.html
    'template_object_name' : 'entry_list', #OVER WRITE THE default name latest
 }
+entry_info_dict_year = {
+   'queryset' : Entry.objects.all(),
+   'date_field': 'pub_date',
+   'make_object_list' : True,
+}
+
+link_info_dict = {
+   'queryset' : Link.objects.all(),
+   'date_field': 'pub_date',
+}
+link_info_dict_year = {
+   'queryset' : Link.objects.all(),
+   'date_field': 'pub_date',
+   'make_object_list' : True,
+}
 
 urlpatterns = patterns('django.views.generic.date_based',
     #(r'^weblog/$', 'coltrane.views.entries_index'),
-    (r'^$', 'archive_index',entry_info_dict1),
+    (r'^$', 'archive_index',entry_info_dict_index,'coltrane_entry_archive_index'),
+    (r'^(?P<year>\d{4})/$', 'archive_year', entry_info_dict_year,'coltrane_entry_archive_year'),
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/$', 'archive_month', entry_info_dict,'coltrane_entry_archive_month'),
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'archive_day', entry_info_dict,'coltrane_entry_archive_day'),
     #(r'^weblog/weblog/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'coltrane.views.entry_detail'),
     # the following is using the generic view to repeat the above task
-    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', entry_info_dict,'coltrane_entry_detail'), #using entry_detail.html
+    (r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', entry_info_dict,'coltrane_entry_detail'), #using entry_detail.html   
+    ## for link 
+    (r'^links/$', 'archive_index',link_info_dict,'coltrane_link_archive_index'),
+    (r'^links/(?P<year>\d{4})/$', 'archive_year', link_info_dict_year,'coltrane_link_archive_year'),
+    (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/$', 'archive_month', link_info_dict,'coltrane_link_archive_month'),
+    (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'archive_day', link_info_dict,'coltrane_link_archive_day'),
+    (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'object_detail', link_info_dict,'coltrane_link_detail'),   
+
+)
+
+urlpatterns += patterns('',
+     (r'^categories/$', 'django.views.generic.list_detail.object_list',{'query_set':Category.objects.all()}),
+     (r'^categories/(?P<slug>[-\w]+)/$', 'coltrane.views.category_detail'),
 )
--- Codes/django/coltrane/views.py
+++ Codes/django/coltrane/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from coltrane.models import Entry
+from coltrane.models import Entry, Category
+from django.views.generic.list_detail import object_list
 
 def entries_index(request):
     return render_to_response('coltrane/entry_index.html', {'entry_list' : Entry.objects.all()})
@@ -13,3 +14,14 @@ def entry_detail(request,year,month,day,slug):
                              pub_date__day= pub_date.day,
                              slug = slug)
    return render_to_response('coltrane/entry_detail.html', {'entry':entry})
+
+def category_list(request):
+    return render_to_response('coltrane/category_list.html', {'object_list' : Category.objects.all()})
+
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    #return render_to_response('coltrane/category_detail.html',
+                              { 'object_list': category.entry_set.all(),
+                                 'category': category })
+    return object_list(request, queryset=category.entry_set.all(),
+                       extra_context={ 'category': category }))
